73b556de655e0ba0c9cc7ff3824a28fa3f078c3c
madnywebtech786 <madnywebtech@gmail.com>
2025-05-08 18:30:18 -0600

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+  $(document).ready(function(){
+    $('.slick-slider').slick({
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: true,
+      dots: true,
+      infinite: true,
+      speed: 1000,
+      fade: true,
+      cssEase: 'linear'
+    });
+  });
+  document.addEventListener("DOMContentLoaded", () => {
+    const animatedElements = document.querySelectorAll(".animate-on-scroll");
+  
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target); // run animation only once
+        }
+      });
+    }, {
+      threshold: 0.3
+    });
+  
+    animatedElements.forEach(el => observer.observe(el));
+  });
+  
