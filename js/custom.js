227ce9bdb8cd660bcc28527957248145b2509f4b
madnywebtech786 <madnywebtech@gmail.com>
2025-05-20 16:52:17 -0600

    index page completed

--- js/custom.js
+++ js/custom.js
@@ -26,12 +26,14 @@
   
     animatedElements.forEach(el => observer.observe(el));
   });
- $(document).ready(function () {
-      $('.testimonial-slider').slick({
-        autoplay: true,
-        autoplaySpeed: 4000,
-        dots: true,
-        arrows: false,
-        adaptiveHeight: true
-      });
+  $(document).ready(function(){
+    $('.testimonial-slider').slick({
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: false,
+      dots: false,
+      fade: true,
+      speed: 800,
+      infinite: true
     });
+  });
